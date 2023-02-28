499f7e3cbab5b200dbb1c818cbbc1893341bc48f
DemonJK <dominikjakovic@mail.de>
2023-02-28 10:01:15 +0100

    NEW 28.02.2023 10:01:15

--- Scene2.js
+++ Scene2.js
@@ -14,8 +14,8 @@ class Scene2 extends Phaser.Scene {
         /*this.load.image("boden", "assets/images/HillsLayer05.png");*/
         this.load.image("background6", "assets/images/HillsLayer06.png");
         console.log("Scene2 Background loaded");
-        this.load.image("boden", "assets/images/HillsLayer05.png");
-        this.load.spritesheet("playermodel", "/assets/SpriteSheet_P.Model/Warrior_Sheet-Effect.png", {frameWidth: 32, frameHeight: 48});
+        this.load.image("boden", "assets/ground/boden_welt_part1.png");
+        this.load.spritesheet("playermodel", "/assets/SpriteSheet_P.Model/Warrior_Sheet-Effect.png", { frameWidth: 48, frameHeight: 72 });
         console.log("playermodel LOADED");
 
     }
@@ -45,6 +45,18 @@ class Scene2 extends Phaser.Scene {
         this.background4.setOrigin(0, 0);
         this.background4.setScale(4.5);
 
+        //PLATFORM (BODEN)
+
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(0, 0, "boden").setOrigin(0, -15).setScale(4.5).refreshBody();
+
+        console.log("PLAYER SPAWNING");
+        this.player = this.physics.add.sprite(100, 1000, "playermodel");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, platforms);
+        console.log("PLAYER SPAWNED");
+
         this.background5 = this.add.image(0, 0, "background5");
         this.background5.setOrigin(0, 0);
         this.background5.setScale(4.5);
@@ -60,17 +72,8 @@ class Scene2 extends Phaser.Scene {
         });
         console.log("TEXT LOADED");
 
-        this.platforms = this.physics.add.group();
-        this.ground = this.physics.add.staticImage(100, 400, "boden");
-        this.ground.refreshBody();
-
-
-        this.player = this.physics.add.sprite(100, 450, "playermodel");
 
         console.log("ENDE DES CODES VON SCENE2");
-        
-    }   
-
-
+    }
 }
 
--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 400 },
             debug: true
         }
     },
