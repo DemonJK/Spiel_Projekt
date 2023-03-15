012e4a3cdc3f75591244c1b9ac1ab312c348f621
DemonJK <dominikjakovic@mail.de>
2023-03-15 09:18:59 +0100

    NEW 15.03.2023 09:18

--- Scene2.js
+++ Scene2.js
@@ -229,36 +229,6 @@ class Scene2 extends Phaser.Scene {
         //
         //
 
-        /*
-        if (!(Math.round(this.player.x) === Math.round(this.enemy.x - 197))) {
-            if (this.player.x < this.enemy.x) {
-                if (this.enemy.body.touching.down) {
-                    this.enemy.body.setOffset(67, 65);
-                    this.enemy.setVelocityX(-55);
-                    console.log("Player X: " + Math.round(this.player.x) + " Enemy X: " + (Math.round(this.enemy.x - 197)));
-                    this.enemy.anims.play("run-left", true);
-                }
-            }
-        } else {
-            this.enemy.body.setOffset(97, 67);
-            this.enemy.anims.play("stand", true)
-        }
-
-        if (!(Math.round(this.enemy.x) === Math.round(this.player.x - 193))) {
-            if (this.player.x > this.enemy.x) {
-                if (this.enemy.body.touching.down) {
-                    this.enemy.body.setOffset(97, 65);
-                    this.enemy.setVelocityX(55).setFlipX(0);
-                    console.log("Enemy X: " + Math.round(this.enemy.x) + " Player X: " + (Math.round(this.player.x - 193)));
-                    this.enemy.anims.play("run-left", true);
-                }
-            }
-        } else {
-            this.enemy.body.setOffset(97, 67);
-            this.enemy.anims.play("stand", true)
-        }
-        */
-
         if (!(Math.round(this.player.x) === Math.round(this.enemy.x - 197))) {
             if (this.player.x < this.enemy.x && this.enemy.body.touching.down || this.player.x > this.enemy.x && this.enemy.body.touching.down) {
                 if (this.player.x < this.enemy.x && this.enemy.body.touching.down) {
