33cdfcb9715628bd284caab228735df532ce8c1f
DemonJK <dominikjakovic@mail.de>
2023-06-14 11:33:32 +0200

    NEW 14.06.2023 11:33

--- HealthBarPlayer.js
+++ HealthBarPlayer.js
@@ -1,7 +1,7 @@
 class HealthBarPlayer extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, hp_val) {
         super(scene, x, y, texture)
-        
+
         this.x = x
         this.y = y
         this.scene = scene
@@ -33,7 +33,7 @@ class HealthBarPlayer extends Phaser.GameObjects.Sprite {
             this.hp_bars[index].destroy(true)
         }
         this.hp_bars = []
-        
+
         for (let i = 0; i < this.hp_val; i++) {
             this.healthBar2 = this.scene.add.rectangle(this.x - 54 + i, this.y + 2, 1, 22, 0xFF0000).setScrollFactor(0, 0).setDepth(0)
             this.hp_bars.push(this.healthBar2)
@@ -44,7 +44,7 @@ class HealthBarPlayer extends Phaser.GameObjects.Sprite {
 
         for (let index = 0; index < amount; index++) {
             this.BlockTop = this.scene.add.rectangle(this.BlockTop.x + 1, this.y - 8, 1, 4, 0x000000).setScrollFactor(0, 0).setDepth(1)
-            this.BlockBottom = this.scene.add.rectangle(this.BlockBottom.x +1 , this.y + 14, 1, 4, 0x000000).setScrollFactor(0, 0).setDepth(1)
+            this.BlockBottom = this.scene.add.rectangle(this.BlockBottom.x + 1, this.y + 14, 1, 4, 0x000000).setScrollFactor(0, 0).setDepth(1)
         }
 
         this.BlockEnd.setPosition(this.BlockTop.x, this.BlockEnd.y)
