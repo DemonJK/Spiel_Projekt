dfcce01447663c58773afc729055738529696411
DemonJK <dominikjakovic@mail.de>
2023-06-21 11:41:28 +0200

    NEW 21.06.2023 11:41

--- TraderNPC.js
+++ TraderNPC.js
@@ -3,29 +3,33 @@ class TraderNPC1 extends Phaser.Physics.Arcade.Sprite {
         super(scene, x, y, texture)
 
         this.TraderNPC1_Config = {
-            idle: "TraderIdleAnim",
-            offsetX: 23.5,
-            offsetY: -12,
-            sizeWidth: 47,
-            sizeHeight: 91,
-            pushable: false,
-            interactionRectX: 15,
-            interactionRectY: -28.5,
-            interactionCheckRectX: 15,
-            interactionCheckRectY: -28.5
+            idle: "TraderIdleAnim",         // IDLE ANIM
+            offsetX: 23.5,                  // OFFSET X VOM TRADER
+            offsetY: -12,                   // OFFSET Y VOM TRADER
+            sizeWidth: 47,                  // WIDTH VOM TRADER
+            sizeHeight: 91,                 // HEIGHT VOM TRADER
+            pushable: false,                // PUSHABLE
+            interactionRectX: 15,           // INTERACTION RECHTECK X
+            interactionRectY: -28.5,        // INTERACTION RECHTECK Y
+            interactionCheckRectX: 15,      // INTERCATION CHECK RECHTECK X
+            interactionCheckRectY: -28.5    // INTERCATION CHECK RECHTECK Y
         }
 
-        this.TraderCheckInteract = this.TraderCheckInteract
-        this.TraderInteract = this.TraderInteract
+        this.TraderCheckInteract = this.TraderCheckInteract // VAR FÜR TRADER-CHECK-INTERACT
+        this.TraderInteract = this.TraderInteract           // VAR FÜR TRADER-INTERACT
 
-        this.SpawnAttributes()
-        this.CollisionBoxes()
-        this.TraderCheckInteractFunction()
-        this.TraderInteractFunction()
+        this.keyE = this.scene.input.keyboard.addKey("E") // INTERAGIEREN TASTE E
+        this.keyS = this.scene.input.keyboard.addKey("S") // ITEMS VERKAUFEN
 
-        this.anims.play(this.TraderNPC1_Config.idle, true)
+        this.SpawnAttributes()              // SPAWN ATTRIBUTE VOM TRADER => CHECK FUNCTION
+        this.CollisionBoxes()               // COLLISION VOM TRADER => CHECK FUNCTION
+        this.TraderCheckInteractFunction()  // INTERACTION CHECK => CHECK FUNCTION
+        this.TraderInteractFunction()       // INTERACTION => CHECK FUNCTION
+
+        this.anims.play(this.TraderNPC1_Config.idle, true)  // IDLE TRADER ANIMATION
     }
 
+    // COLLISION VOM TRADER
     CollisionBoxes() {
         this.scene.physics.add.collider(this, this.scene.buildings3)
 
@@ -42,14 +46,16 @@ class TraderNPC1 extends Phaser.Physics.Arcade.Sprite {
         })
     }
 
+    // SPAWN ATTRIBUTE
     SpawnAttributes() {
-        this.scene.add.existing(this)
-        this.scene.physics.world.enable(this)
-        this.setPushable(this.TraderNPC1_Config.pushable)
+        this.scene.add.existing(this)                       // ERSTELLT DENN TRADER IN BESTIMMTER SZENE
+        this.scene.physics.world.enable(this)               // AKTIVIERT DIE PHYSICS FÜR DENN TRADER
+        this.setPushable(this.TraderNPC1_Config.pushable)   // SETZT DENN TRADER AUF PUSHABLE = FALSE
         this.body.setSize(this.TraderNPC1_Config.sizeWidth, this.TraderNPC1_Config.sizeHeight)
         this.setOffset(this.TraderNPC1_Config.offsetX, this.TraderNPC1_Config.offsetY)
     }
 
+    // INTERACTION CHECK
     TraderCheckInteractFunction() {
         this.TraderCheckInteract = this.scene.add.rectangle(
             this.TraderNPC1_Config.interactionRectX,
@@ -62,6 +68,7 @@ class TraderNPC1 extends Phaser.Physics.Arcade.Sprite {
         this.scene.physics.add.existing(this.TraderCheckInteract, true).setDepth(-1)
     }
 
+    // INTERACT
     TraderInteractFunction() {
         this.TraderInteract = this.scene.add.rectangle(
             this.TraderNPC1_Config.interactionRectX,
@@ -74,22 +81,33 @@ class TraderNPC1 extends Phaser.Physics.Arcade.Sprite {
         this.scene.physics.add.existing(this.TraderInteract, true).setDepth(-1)
     }
 
+    // TRADER MENU
+    TraderMenuFunction() {
+        //EINPFLEGUNG VON TRADER MENU
+        console.log("TRADER MENU OPENED");
+    }
+
     update() {
         this.TraderCheckInteract.body.x = this.x + this.TraderNPC1_Config.interactionCheckRectX + this.TraderInteract.width
         this.TraderCheckInteract.body.y = this.y + this.TraderNPC1_Config.interactionCheckRectY
         this.TraderInteract.body.x = this.x + this.TraderNPC1_Config.interactionRectX
         this.TraderInteract.body.y = this.y + this.TraderNPC1_Config.interactionRectY
-        
+
         this.overlapCheck = this.scene.physics.overlap(this.TraderCheckInteract, this.scene.player)
         if (this.overlapCheck === true) {
-            console.log("In Check");
+            //console.log("In Check");
             ///// ---------
             if (this.scene.physics.overlap(this.TraderInteract, this.scene.player)) {
-                console.log("In Interact"); // AFTER CHECK
+                if (this.keyE.isDown && this.scene.player.body.touching.down) {
+                    this.scene.player.setVelocityX(0)
+                    this.TraderMenuFunction()
+                    console.log("KEY E PRESSED");
+                }
+                //console.log("In Interact"); // AFTER CHECK
             }
             ///// ---------
         } else {
-            console.log("Not in Check");
+            //console.log("Not in Check");
         }
     }
 }
